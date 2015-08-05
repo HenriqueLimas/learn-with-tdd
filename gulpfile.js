30853383d6af0c1c02ee754b69ee724dcb18f35f
henrique.limas <henrique.lima@betha.com.br>
2015-08-05 11:54:04 -0300

    core(gulpfile): change tab to spaces.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var karma = require('karma').Server;
 
 gulp.task('js', function(done) {
-	new karma({
-		configFile: __dirname + '/js/karma.conf.js'
-	}, done).start();
+  new karma({
+    configFile: __dirname + '/js/karma.conf.js'
+  }, done).start();
 });
