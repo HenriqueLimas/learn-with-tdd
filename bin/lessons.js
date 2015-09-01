88dd9d1b4d6eacee1a6e6df49d8242ae042fb1ea
Henrique Ramos Limas <henrique.ramos.limas@gmail.com>
2015-09-01 17:16:47 -0300

    chore(ltdd): remove gulp dependencies.

---
+++ bin/lessons.js
@@ -0,0 +1,44 @@
+module.exports = {
+  js: {
+    lesson1: {
+      files: ['lesson-01/*.js'],
+      title: ''
+    },
+    lesson2: {
+      files: ['lesson-02/*.js'],
+      title: ''
+    },
+    lesson3: {
+      files: ['lesson-03/*.js'],
+      title: ''
+    },
+    lesson4: {
+      files: ['lesson-04/*.js'],
+      title: ''
+    },
+    lesson5: {
+      files: ['lesson-05/*.js'],
+      title: ''
+    },
+    lesson6: {
+      files: ['lesson-06/*.js'],
+      title: ''
+    },
+    lesson7: {
+      files: ['lesson-07/*.js'],
+      title: ''
+    },
+    lesson8: {
+      files: ['lesson-08/*.js'],
+      title: ''
+    },
+    lesson9: {
+      files: ['lesson-09/*.js'],
+      title: ''
+    },
+    lesson10: {
+      files: ['lesson-10/*.js'],
+      title: ''
+    },
+  }
+};
--- bin/ltdd.js
+++ bin/ltdd.js
@@ -2,25 +2,28 @@
 
 'use strict';
 
-var spawn = require('child_process').spawn;
 var cwd = process.env.PWD || process.cwd();
+
+var extend = require('extend');
+
+var karma = require('karma').Server;
+var karmaConfig = {
+  configFile: cwd + '/js/karma.conf.js'
+};
+
+var lessons = require('./lessons');
 
 var userArgs = process.argv.slice(2);
 
 var course = userArgs[0];
 var lesson = userArgs[1];
 
-var taskName = course + ':' + lesson;
-var gulpProcess = spawn('gulp', [taskName], {
-  stdio: 'inherit',
-  cwd: cwd
-});
+var lessonConfig = lessons[course][lesson];
 
-gulpProcess.on('close', function(code) {
-  process.exit(code);
-});
+var karmaProcess = new karma(extend(karmaConfig, {
+  files: lessonConfig.files
+})).start();
 
 process.on('uncaughtException', function(err) {
   console.err(err.stack);
-  gulpProcess.kill('SIGHUP');
 });
